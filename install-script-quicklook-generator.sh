441d8ce69760fe7791c533baec3e3266beb86bc9
Gaël Blanchemain <anegg0@users.noreply.github.com>
2024-05-12 19:06:43 -0700

    fix: updated binary url as raw

--- install-script-quicklook-generator.sh
+++ install-script-quicklook-generator.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 # Correct binary download URL from the release assets
-BINARY_URL="https://github.com/anegg0/quicklook-generator/releases/download/domolites/quicklook-generator"
+BINARY_URL="https://github.com/anegg0/quicklook-generator/raw/main/dist/quicklook-generator"
 
 BINARY_NAME="quicklook-generator"
 
